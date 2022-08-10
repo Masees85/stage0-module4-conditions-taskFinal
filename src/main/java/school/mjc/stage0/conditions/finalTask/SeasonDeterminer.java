90d951d00ff232b9326d8bb193f29c2b4aabcc26
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-10 14:31:54 -0400

    Update SeasonDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -1,6 +1,41 @@
 package school.mjc.stage0.conditions.finalTask;
 
-public class SeasonDeterminer {
-    public void tellTheSeason(int monthNumber) {
+public class SeasonDeterminer 
+
+{
+    public void tellTheSeason(int monthNumber) 
+    
+    {
+        
+                 if(month>=1 && month<=12)
+        {
+            
+              if (month>=6 && month<=8)
+        {
+            System.out.println("Summer");
+        }
+              if (month>=9 && month<=11)
+        {
+            System.out.println("Fall");
+        }
+        
+     
+        
+        if(month==12 || month==1 || month==2)
+        {
+            System.out.println("Winter");
+        }
+        
+        if(month>=3 && month<=5)
+        {
+            System.out.println("Spring");
+        }
+        }       
+                           
+        else
+        {
+            System.out.println("Wrong month number");
+        }
+
     }
 }
